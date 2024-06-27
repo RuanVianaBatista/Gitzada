d987e7889d23e7d853707527dfa371e848df2687
RuanVianaBatista <ruanvianabatista@gmail.com>
2024-06-27 14:58:14 -0300

    att3

--- pythonnoGit.py
+++ pythonnoGit.py
@@ -1,8 +1,10 @@
 print('Nussa')
 n = int(input('Digite um numero: '))
 pares = 0
+impares = 0
 if n % 2 == 0:
     print('O numero é par')
     pares += 1
 else:
     print('O numero é impar')
+    impares += 1
